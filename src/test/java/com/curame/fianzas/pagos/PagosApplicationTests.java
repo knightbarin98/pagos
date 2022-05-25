b0b00c2b01fdf5e4271419d0a3df0aeb45793279
ivansoto <lopezsotoivan01@gmail.com>
2022-05-24 21:09:12 -0500

    first commit

---
+++ src/test/java/com/curame/fianzas/pagos/PagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curame.fianzas.pagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
